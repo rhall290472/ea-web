ef3cd1490c196fd8b038f4d9279309b642f1f46e
rhall290472 <56454660+rhall290472@users.noreply.github.com>
2026-01-25 14:17:38 -0700

    Formatted code

--- public/src/delete.php
+++ public/src/delete.php
@@ -3,33 +3,33 @@
 require_once __DIR__ . '/../../config.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST' || empty($_POST['filename'])) {
-    header('Location: view.php');
-    exit;
+  header('Location: view.php');
+  exit;
 }
 
 $filename = $_POST['filename'];
 $filepath = DATA_DIR . '/' . $filename;
 
 if (file_exists($filepath)) {
-    $data = json_decode(file_get_contents($filepath), true);
-    $SEA_id = $data['id'] ?? 'unknown';
+  $data = json_decode(file_get_contents($filepath), true);
+  $SEA_id = $data['id'] ?? 'unknown';
 
-    // Delete main JSON
-    unlink($filepath);
+  // Delete main JSON
+  unlink($filepath);
 
-    // Delete uploads folder
-    $uploadDir = UPLOAD_DIR . '/' . $SEA_id;
-    if (is_dir($uploadDir)) {
-        array_map('unlink', glob($uploadDir . '/*'));
-        rmdir($uploadDir);
-    }
+  // Delete uploads folder
+  $uploadDir = UPLOAD_DIR . '/' . $SEA_id;
+  if (is_dir($uploadDir)) {
+    array_map('unlink', glob($uploadDir . '/*'));
+    rmdir($uploadDir);
+  }
 
-    // Delete history
-    $historyDir = DATA_DIR . '/history/' . $SEA_id;
-    if (is_dir($historyDir)) {
-        array_map('unlink', glob($historyDir . '/*'));
-        rmdir($historyDir);
-    }
+  // Delete history
+  $historyDir = DATA_DIR . '/history/' . $SEA_id;
+  if (is_dir($historyDir)) {
+    array_map('unlink', glob($historyDir . '/*'));
+    rmdir($historyDir);
+  }
 }
 
 header('Location: view.php?deleted=1');
