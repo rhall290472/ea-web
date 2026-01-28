7598a86dd0da44c6f577880881323b488941701f
rhall290472 <56454660+rhall290472@users.noreply.github.com>
2026-01-28 14:54:39 -0700

    FIxed "Network error"

--- public/src/submit.php
+++ public/src/submit.php
@@ -1,7 +1,17 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$customLogFile = __DIR__ . '/mpdf-errors.log';  // Creates mpdf-errors.log INSIDE /src/
+
+ini_set('log_errors', 1);
+ini_set('error_log', $customLogFile);
+
 // src/submit.php
 require_once __DIR__ . '/../../config.php';
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+
 
 use Mpdf\Mpdf;
 
